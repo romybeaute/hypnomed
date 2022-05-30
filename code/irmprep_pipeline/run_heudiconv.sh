f8bc5bfcb6d4a005ef18e7036bbe820cc3f52540
romybeaute <romybeaute.univ@gmail.com>
2022-05-30 21:58:28 +0200

    modif heudi

--- code/irmprep_pipeline/run_heudiconv.sh
+++ code/irmprep_pipeline/run_heudiconv.sh
@@ -8,25 +8,25 @@
 set -eu
 
 # where we want to output the data
-OUTDIR='/mnt/data/sebastien/LONGIMED/BIDS/'
+OUTDIR='/mnt/data/romy/hypnomed/MRI_raw/BIDS/'
 # where to find the heurtic.py file for heudiconv
-HEURISTIC_PATH='/mnt/data/sebastien/LONGIMED/BIDS/code/heuristic.py'
+HEURISTIC_PATH='/mnt/data/romy/hypnomed/MRI_raw/BIDS/code/heuristic.py'
 # where the DICOMs are located
-DCMROOT='/mnt/data/sebastien/LONGIMED/RAW_DATA/DICOM'
+DCMROOT='/mnt/data/romy/hypnomed/MRI_raw/DICOM'
 # receive all directories, and index them per job array
 SUBJDIRS=(${@:1})
 SUBJDIR=${SUBJDIRS[${SLURM_ARRAY_TASK_ID}]}
 
 SESSDIRS=(`find $DCMROOT -mindepth 3 -maxdepth 3  -name ses* -type d`)
 
-echo Submitted directory: ${SUBJDIR:48}
+echo Submitted directory: ${SUBJDIR:42}
 
-IMG="/mnt/data/sebastien/singularity_images/heudiconv.sif"
+IMG="/mnt/data/romy/singularity_images/heudiconv_0.5.4.sif"
 
-SESSION=(001 002 003)
+SESSION=(001)
 
 for SESSION_ID in ${SESSION[@]} ; do
-    CMD="singularity run -B ${DCMROOT}:/base:ro -B ${OUTDIR}:/output -e ${IMG} -d /base/sub-{subject}/ses-{session}/*/*.dcm -o /output -f /output/code/heuristic.py -c dcm2niix -b --overwrite -s ${SUBJDIR:48} -ss $SESSION_ID"
+    CMD="singularity run -B ${DCMROOT}:/base:ro -B ${OUTDIR}:/output -e ${IMG} -d /base/sub-{subject}/ses-{session}/*/*.dcm -o /output -f /output/code/heuristic.py -c dcm2niix -b --overwrite -s ${SUBJDIR:42} -ss $SESSION_ID"
     printf "Command:\n${CMD}\n"
     ${CMD}
     echo "Successful process"
