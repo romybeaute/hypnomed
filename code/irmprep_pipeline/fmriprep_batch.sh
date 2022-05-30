ca744d1ab98f3ff2a1060f5c359eaff18bb77819
romybeaute <romybeaute.univ@gmail.com>
2022-05-30 21:50:20 +0200

    modifier fmriprep

--- code/irmprep_pipeline/fmriprep_batch.sh
+++ code/irmprep_pipeline/fmriprep_batch.sh
@@ -6,17 +6,15 @@
 #SBATCH --mem-per-cpu=10G
 #SBATCH --array=1-60
 
-BIDSDIR='/mnt/data/sebastien/LONGIMED/BIDS/'
-OUTDIR='/mnt/data/sebastien/LONGIMED/recon_all'
-IMG='/mnt/data/sebastien/singularity_images/fmriprep.sif'
+BIDSDIR='/mnt/data/romy/hypnomed/MRI_raw/BIDS/'
+OUTDIR='/mnt/data/romy/hypnomed/recon_all'
+IMG='/mnt/data/romy/singularity_images/fmriprep.simg'
 
 SUBJ=(`find $BIDSDIR -mindepth 1 -maxdepth 1  -name sub* -type d`)
 
-echo ${SUBJ[$SLURM_ARRAY_TASK_ID-1]:48} 
+echo ${SUBJ[$SLURM_ARRAY_TASK_ID-1]:42} 
 singularity run --cleanenv -B /mnt:/mnt $IMG $BIDSDIR $OUTDIR participant\
-      --participant-label ${SUBJ[$SLURM_ARRAY_TASK_ID-1]:48} --low-mem --stop-on-first-crash \
+      --participant-label ${SUBJ[$SLURM_ARRAY_TASK_ID-1]:42} --low-mem --stop-on-first-crash \
       --use-aroma --cifti-output --notrack \
-      --output-spaces fsaverage5 --fs-license-file /mnt/data/sebastien/packages/license.txt \
+      --output-spaces fsaverage5 --fs-license-file /mnt/data/romy/packages/license.txt \
       --omp-nthreads 9 --nthreads 9 --mem_mb 10000
-
-
