95fe899674240a3ae4d889f9ce7b5bdcca65895b
romybeaute <romybeaute.univ@gmail.com>
2022-06-20 13:43:12 +0200

    execute

--- code/irmprep_pipeline/execute_fmriprep.sh
+++ code/irmprep_pipeline/execute_fmriprep.sh
@@ -1,4 +1,4 @@
-export STUDY=/mnt/data/sebastien/LONGIMED/
-export PARTCIPANTS_INFO=$STUDY/raw_data/BIDS/participants.tsv
+export STUDY=/mnt/data/romy/hypnomed/
+export PARTCIPANTS_INFO=$STUDY/MRI_raw/BIDS/participants.tsv
 
 sbatch --array=1-$(( $( wc -l $PARTCIPANTS_INFO | cut -f1 -d' ' ) - 1 )) fmriprep_slurm.sh
