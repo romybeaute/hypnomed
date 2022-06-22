52688e1587dea37766b7f4eb0f8c29fea42c5dff
romybeaute <romybeaute.univ@gmail.com>
2022-06-22 17:07:24 +0200

    changed fieldmap

--- code/irmpreproc_seb/BIDS_fieldmap.py
+++ code/irmpreproc_seb/BIDS_fieldmap.py
@@ -3,11 +3,6 @@
 """
 Created on Thu Jul  8 11:48:54 2021
 
-
-###########
-#permet d'ajouter les champs manquants dans les .json des fichiers fmaps d'après les spécifications BIDS pour qu'ils soient utilisables par fmriprep.
-###########
-
 * modify the .json files of fieldmaps files to use them in fmriprep
 * see for ref. https://bids-specification.readthedocs.io/en/stable/04-modality-specific-files/01-magnetic-resonance-imaging-data.html#types-of-fieldmaps
 * this script corresponds to the case 1 from the above link
@@ -18,13 +13,14 @@ import json
 
 working_path = '/mnt/data/sebastien/LONGIMED/raw_data/BIDS'
 
-def get_func_nii(path):
+def get_func_nii(path,ses_folder):
     #return a list with all the functional nifti files
     #it assumes that the fieldmap collected can be used for all of them
     nii_bold_files = []
     for file in os.listdir(path):
         if ('bold' in file) & ('nii.gz' in file):
-            nii_bold_files.append(file)
+            
+            nii_bold_files.append(os.path.join(ses_folder,'func',file))
     return nii_bold_files
 
 for sub_folder in os.listdir(working_path):
@@ -42,7 +38,8 @@ for sub_folder in os.listdir(working_path):
                     file_path = os.path.join(fmap_path,file)
                     with open(file_path, 'r', encoding='utf-8') as f:
                         data = json.load(f)
-                        data.update({"IntendedFor":get_func_nii(func_path)})
+                        
+                        data.update({"IntendedFor":get_func_nii(func_path,ses_folder)})
                         if 'phasediff' in file:
                             data.update({"B0FieldIdentifier": "phasediff_fmap0"})
                     with open(file_path, 'w', encoding='utf-8') as json_file:
