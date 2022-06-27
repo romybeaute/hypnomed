0194b45265a0aaff33aab60fc7ae683a29a002ac
romybeaute <romybeaute.univ@gmail.com>
2022-06-27 14:51:25 +0200

    comb

--- diffusion_embedding/combine_subjects_all.py
+++ diffusion_embedding/combine_subjects_all.py
@@ -18,7 +18,7 @@ def run_realign(emb, tar, firstpass = False):
 
 
 def load_template(template_path):
-    cortex = loadmat('/mnt/data/romy/hypnomed/git/diffusion_embedding/data/cortex.mat')
+    cortex = loadmat('/mnt/data/romy/hypnomed/git/data/cortex.mat')
     #('/mnt/data/sebastien/data/cortex.mat')
     cortex = np.squeeze(cortex['cortex'])
     template = np.zeros([20484,5])
@@ -32,12 +32,12 @@ def load_template(template_path):
     return template
 
 # df = pd.read_csv('/mnt/data/sebastien/scripts/subject_list.txt', header=None)
-df = pd.read_csv('/mnt/data/sebastien/hypnomed/git/diffusion_embedding/data/subject_list.txt', header=None)
+df = pd.read_csv('/mnt/data/romy/hypnomed/git/data/subject_list.txt', header=None)
 sublist = np.asarray(df).flatten()
 
 # data_folder = '/mnt/data/sebastien/diffusion_embedding_step/emb_output'
 data_folder = '/mnt/data/hypnomed/git/diffusion_embedding/emb_output'
-template = load_template('/mnt/data/romy/hypnomed/git/diffusion_embedding/data/template/fsaverage')
+template = load_template('/mnt/data/romy/hypnomed/git/data/template/fsaverage')
 #for state in ["compassion","openmonitoring","restingstate"]:
 #for state in ["rs1","rs2","rs3"]:
 embeddings = []
