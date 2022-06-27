831912c05455d032938dfb4e130d5ac63cc555dc
romybeaute <romybeaute.univ@gmail.com>
2022-06-27 15:40:07 +0200

    scripts

---
+++ diffusion_embedding/scripts/combine_subjects_all.py
@@ -0,0 +1,68 @@
+import nibabel as nib
+import numpy as np
+import os, glob
+import pandas as pd
+from scipy.io import savemat
+from scipy.io import loadmat
+from nilearn import surface
+
+def run_realign(emb, tar, firstpass = False):
+    realign = []
+    if firstpass:
+        realign.append(tar)
+    for i, embed in enumerate(emb):
+        u, s, v = np.linalg.svd(tar.T.dot(embed), full_matrices=False)
+        xfm = v.T.dot(u.T)
+        realign.append(embed.dot(xfm))
+    return realign
+
+
+def load_template(template_path):
+    cortex = loadmat('/mnt/data/romy/hypnomed/git/data/cortex.mat')
+    #('/mnt/data/sebastien/data/cortex.mat')
+    cortex = np.squeeze(cortex['cortex'])
+    template = np.zeros([20484,5])
+    for grad_id in range(5):
+        template_L = surface.load_surf_data(os.path.join(template_path,'hcp.embed.grad_'+str(grad_id+1)+'.L.fsa5.func.gii'))
+        template_R = surface.load_surf_data(os.path.join(template_path,'hcp.embed.grad_'+str(grad_id+1)+'.R.fsa5.func.gii'))
+        template[:,grad_id] = np.hstack([template_L,template_R])
+    template = template[cortex,:]
+    template = template - np.mean(template,0)
+    template = template/np.std(template,0)
+    return template
+
+# df = pd.read_csv('/mnt/data/sebastien/scripts/subject_list.txt', header=None)
+df = pd.read_csv('/mnt/data/romy/hypnomed/git/data/subject_list.txt', header=None)
+sublist = np.asarray(df).flatten()
+
+# data_folder = '/mnt/data/sebastien/diffusion_embedding_step/emb_output'
+data_folder = '/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_output'
+template = load_template('/mnt/data/romy/hypnomed/git/data/template/fsaverage')
+#for state in ["compassion","openmonitoring","restingstate"]:
+#for state in ["rs1","rs2","rs3"]:
+embeddings = []
+subs = []
+for s in sublist:
+    try:
+        subs.append(s)
+        # embeddings.append(np.load(data_folder+'/embedding_dense_emb.%s.compassion.npy' % s))
+        # embeddings.append(np.load(data_folder+'/embedding_dense_emb.%s.openmonitoring.npy' % s))
+        # embeddings.append(np.load(data_folder+'/embedding_dense_emb.%s.restingstate.npy' % s))
+        embeddings.append(np.load(data_folder+'/embedding_dense_emb.%s.rs1.npy' % s))
+        embeddings.append(np.load(data_folder+'/embedding_dense_emb.%s.rs2.npy' % s))
+        embeddings.append(np.load(data_folder+'/embedding_dense_emb.%s.rs3.npy' % s))
+        print(s)
+    except:
+        print(s)
+
+    # np.shape(embeddings)
+
+realigned = run_realign(embeddings[1:], template, firstpass=True)
+for i in range(5):
+    realigned = run_realign(realigned, np.asarray(np.mean(realigned, axis=0).squeeze()), firstpass = False)
+
+savemat(data_folder+'/rs1_rs2_r3_group_embedding_new.mat', mdict={'emb': realigned, 'subs': subs})
+    
+f = loadmat(data_folder+'/rs1_rs2_r3_group_embedding_new.mat')
+print(len(f['subs']))
+print(f['subs'])
---
+++ diffusion_embedding/scripts/pipeline.py
@@ -0,0 +1,58 @@
+
+#!/bin/python
+
+import sys, os
+sys.path.append("/mnt/data/romy/hypnomed/git/diffusion_embedding") #("/mnt/data/sebastien/diffusion_embedding_step/")
+from load_fs import load_fs
+import numpy as np
+import nibabel as nib
+from mapalign import embed
+from numba import jit
+from scipy.sparse.linalg import eigsh
+from scipy import sparse
+
+@jit(parallel=True)
+def run_perc(data, thresh):
+    perc_all = np.zeros(data.shape[0])
+    for n,i in enumerate(data):
+        data[n, i < np.percentile(i, thresh)] = 0.
+    for n,i in enumerate(data):
+        data[n, i < 0.] = 0.
+    return data
+ 
+def main(subj):
+    for ses in ["ses-001"]:
+    # for ses in ["ses-001", "ses-002", "ses-003"]:
+        for state in ["rs1", "rs2", "rs3"]:
+            if os.path.isfile(f'/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_output/embedding_dense_emb.{subj}.{ses}.{state}.npy'):
+            # if os.path.isfile(f'/mnt/data/sebastien/diffusion_embedding_step/emb_output/embedding_dense_emb.{subj}.{ses}.{state}.npy'):
+
+                emb = np.load(f'/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_output/embedding_dense_emb.{subj}.{ses}.{state}.npy')
+
+            else:
+
+                K = load_fs(subj,ses,state)
+                K[np.isnan(K)] = 0.0
+
+                A_mA = K - K.mean(1)[:,None]
+                ssA = (A_mA**2).sum(1)
+                Asq = np.sqrt(np.dot(ssA[:,None],ssA[None]))
+                Adot = A_mA.dot(A_mA.T)
+
+                K = Adot/Asq
+                del A_mA, ssA, Asq, Adot
+                K = run_perc(K, 90)
+
+                norm = (K * K).sum(0, keepdims=True) ** .5
+                K = K.T @ K
+                aff = K / norm / norm.T
+                del norm, K
+
+                emb, res = embed.compute_diffusion_map(aff, alpha = 0.5, n_components=5, skip_checks=True, overwrite=True, eigen_solver=eigsh, return_result=True)
+                del aff
+
+                np.save(f'/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_output/embedding_dense_emb.{subj}.{ses}.{state}.npy', emb)
+                np.save(f'/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_output/embedding_dense_res.{subj}.{ses}.{state}.npy', res)
+
+if __name__ == "__main__":
+     main(sys.argv[1])
