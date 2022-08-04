2a19ffe2b648dd416d519a184157b5a5790b585e
Romy Beauté <81311719+romybeaute@users.noreply.github.com>
2022-08-04 18:58:13 +0200

    Update pipeline.py

--- diffusion_embedding/scripts_reordered/pipeline.py
+++ diffusion_embedding/scripts_reordered/pipeline.py
@@ -3,6 +3,11 @@
 
 """
 STEP 2 : Perform diffusion embedding / Correlate RS data across sessions and embed
+
+
+Ressources for computing correlation matrix : 
+- https://stackoverflow.com/questions/26524950/how-to-apply-corr2-functions-in-multidimentional-arrays-in-matlab/26526798#26526798
+- https://stackoverflow.com/questions/30143417/computing-the-correlation-coefficient-between-two-multi-dimensional-arrays
 """
 
 import sys, os
@@ -18,13 +23,19 @@ from scipy import sparse
 import pandas as pd
 from data.get_infos import get_rs_condition
 
+
+
+"""
+Transform z to r correlation values with hypobolic tangent function 
+For each raw of the matrix, keep only the values at the top 10% of connections
+"""
 @jit(parallel=True)
 def run_perc(data, thresh):
     perc_all = np.zeros(data.shape[0])
     for n,i in enumerate(data):
-        data[n, i < np.percentile(i, thresh)] = 0.
+        data[n, i < np.percentile(i, thresh)] = 0. #values under the connection threshold (<top 10%) are zeroed
     for n,i in enumerate(data):
-        data[n, i < 0.] = 0.
+        data[n, i < 0.] = 0. #zeroed the rest of voxels with negative connections
     return data
 
 
@@ -44,21 +55,27 @@ def main(subj):
 
             else:
 
-                K = load_fs(subj,ses,state)
+                # CALCULATE FUNCTIONNAL CONNECTIVITY MATRIX (K) FROM NORMALIZED DATA (loaded from load_fs)
+                
+                #Step 1 : load data
+                K = load_fs(subj,ses,state) #load normalized data
                 K[np.isnan(K)] = 0.0
 
-                A_mA = K - K.mean(1)[:,None]
-                ssA = (A_mA**2).sum(1)
+                #Step 2 : Compute correlation coefficient from normalized data K
+                A_mA = K - K.mean(1)[:,None] #Rowwise mean of input arrays & subtract from input arrays themeselves
+                ssA = (A_mA**2).sum(1)  Sum of squares across rows
                 Asq = np.sqrt(np.dot(ssA[:,None],ssA[None]))
                 Adot = A_mA.dot(A_mA.T)
-
-                K = Adot/Asq
+                K = Adot/Asq #finally got K as the correlation coefficient (functional connectivity matrix)
                 del A_mA, ssA, Asq, Adot
-                K = run_perc(K, 90)
+                
+                #Step 3 : Transformation z --> r correlations (scales values b/w [-1;1]
+                K = run_perc(K, 90) #threshold to keep only top 10% connections
 
-                norm = (K * K).sum(0, keepdims=True) ** .5
-                K = K.T @ K
-                aff = K / norm / norm.T
+                #Step 4 : Compute similarity between all pairs of rows using cosine distance (resulting in positive, symmetrix affinity matrix aff)
+                norm = (K * K).sum(0, keepdims=True) ** .5 
+                K = K.T @ K #numerator (dot product)
+                aff = K / norm / norm.T #similarities (full affinity matrix)
                 del norm, K
 
                 emb, res = embed.compute_diffusion_map(aff, alpha = 0.5, n_components=5, skip_checks=True, overwrite=True, eigen_solver=eigsh, return_result=True) #Compute the diffusion maps of a symmetric similarity matrix
