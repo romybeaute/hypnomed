21a19d0c74397fd772ea06c9629f2fe2419589c8
romybeaute <romybeaute.univ@gmail.com>
2022-08-05 17:44:01 +0200

    added function for isolated conditions

--- diffusion_embedding/visualize_emb_output/diffusion_maps.py
+++ diffusion_embedding/visualize_emb_output/diffusion_maps.py
@@ -46,22 +46,22 @@ else: #gradients_for == 'blocks' (controling for order effect)
 
 npy_folder = '/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_outputs/emb_output_{}'.format(gradients_for)
 
-mesh = 'fsaverage5' #the low-resolution fsaverage5 mesh (10242 nodes)
+mesh = 'fsaverage5' #the low-resolution fsaverage5 mesh (10242 nodes = vertices per hemisphere)
 fsaverage_path = '/mnt/data/romy/packages/freesurfer/subjects/{}/label'.format(mesh)
 image_output_folder = '/mnt/data/romy/hypnomed/git/diffusion_embedding/visualize_emb_output/images_gradients/{}'.format(gradients_for) #where to store the visualisations of the gradient(s)
 
 
 
-
-lab_lh = nib.freesurfer.read_label(fsaverage_path+'/lh.cortex.label') #Freesurfer fsaverage surface
-lab_rh = 10242 + nib.freesurfer.read_label(fsaverage_path+'/rh.cortex.label')
+# Load Freesurfer fsaverage surface with read_label : return array with indices of vertices included in label
+lab_lh = nib.freesurfer.read_label(fsaverage_path+'/lh.cortex.label') #shape = (9361,)
+lab_rh = 10242 + nib.freesurfer.read_label(fsaverage_path+'/rh.cortex.label') #shape = (9361,)
 lab= np.concatenate((lab_lh,lab_rh))
 
 
 df = pd.read_csv('/mnt/data/romy/hypnomed/git/diffusion_embedding/scripts/subject_list.txt', header=None) #list of the subjects we have
 
 sublist = np.asarray(df).flatten()
-
+print(len(sublist))
 
 
 
@@ -70,11 +70,13 @@ def make_gradients_images(sublist,emb_condition):
     
     if len(sublist) > 1:
         prefix = 'group' #group-level analysis
+        image_folder = os.path.join(image_output_folder,prefix)
     else:
         prefix = sublist[0] #indiv-level analysis
+        image_folder = os.path.join(image_output_folder+'/indivs',prefix)
     print('Gradient for : ',prefix)
 
-    image_folder = os.path.join(image_output_folder,prefix)
+    
     if not os.path.isdir(image_folder):
         os.makedirs(image_folder)
 
@@ -84,13 +86,21 @@ def make_gradients_images(sublist,emb_condition):
     mat_file = mat_folder+'/{}_{}_embedding.mat'.format(prefix, emb_condition)
 
 
-    if not os.path.isfile(mat_file):
-        select_embedding(emb_condition,sublist,gradients_for) #create .mat embedding file for specified sublist & condition
+    if not os.path.isfile(mat_file): #create .mat embedding file for specified sublist & condition
+        select_embedding(emb_condition,sublist,gradients_for) 
         print('Creating .mat file for sublist : {} under {} condition(s)'.format(sublist,emb_condition))
-
+    
     try:
         b = loadmat(mat_file)
         print('Loading ',mat_file)
+
+        print(70*'-')
+        print('Description {} : '.format(mat_file))
+        for k in b.keys():
+            print('- {} :\n {}\n'.format(k,b[k]))
+        print('- b embedding shape : ',b['emb'].shape)
+        print(70*'-')
+
         b['emb'].shape
         a= np.zeros(20484)
         a[lab]=np.mean(b['emb'],axis=0)[:,0] #check if corresponds to 1st gradient
@@ -99,6 +109,82 @@ def make_gradients_images(sublist,emb_condition):
         print("Gradient image for %s completed" % mat_file)
     except:
         print("/!\ Gradient image for %s failed /!\ " % mat_file)
+
+
+
+
+
+def make_gradients_images_isolated_condition(sublist,indiv_emb_state,plot_n_dims=1):
+
+    
+    if len(sublist) > 1:
+        prefix = 'group' #group-level analysis
+        image_folder = os.path.join(image_output_folder,prefix)
+    else:
+        prefix = sublist[0] #indiv-level analysis
+        image_folder = os.path.join(image_output_folder+'/indivs',prefix)
+    print('Gradient for : ',prefix)
+
+    image_folder = os.path.join(image_output_folder,prefix)
+    if not os.path.isdir(image_folder):
+        os.makedirs(image_folder)
+
+    mat_folder = '/mnt/data/romy/hypnomed/git/diffusion_embedding/emb_matrices/{}'.format(prefix)
+    if not os.path.isdir(mat_folder):
+        os.makedirs(mat_folder)
+    mat_file = mat_folder+'/{}_{}_embedding.mat'.format(prefix, indiv_emb_state)
+
+
+    if not os.path.isfile(mat_file): #create .mat embedding file for specified sublist & condition
+        create_mat_embeddings(indiv_emb_state,sublist,gradients_for)
+        print('Creating .mat file for sublist : {} under {} condition'.format(sublist,indiv_emb_state))
+    
+    try:
+        b = loadmat(mat_file)
+        print('Loading ',mat_file)
+
+        print(70*'-')
+        print('Description {} : '.format(mat_file))
+        for k in b.keys():
+            print('- {} :\n {}\n'.format(k,b[k]))
+        print('- b embedding shape : ',b['emb'].shape)
+        print('     ---> n_subjects = ',b['emb'].shape[0])
+        print('     ---> n_nodes = ',b['emb'].shape[1])
+        print('     ---> n_dimensions = ',b['emb'].shape[2])
+        print(70*'-')
+
+        b['emb'].shape  #(n_subjects,n_nodes,n_dims)
+        n_dims = b['emb'].shape[2]
+        # a = np.zeros(20484) #size concatenated vertices from fsaverage5 template
+        # #mean across subjects
+        # mean_embs = np.mean(b['emb'],axis=0) #mean embeddings for each dimension, across subjects (axis=0 signifies avg across subjects, which is the first dimension of b)
+        # a[lab]=np.mean(b['emb'],axis=0)[:,0] #check if corresponds to 1st gradient
+        # nilearn.plotting.plot_surf_stat_map('/mnt/data/romy/packages/freesurfer/subjects/fsaverage5/surf/lh.inflated',a[:10242],colorbar=True, cmap='jet', vmax=5.5,title='{}_diffusion_map_{}_lh'.format(prefix,indiv_emb_state),output_file=image_folder+'/{}_diffusion_map_{}_lh.png'.format(prefix,emb_condition))
+        # nilearn.plotting.plot_surf_stat_map('/mnt/data/romy/packages/freesurfer/subjects/fsaverage5/surf/rh.inflated',a[10242:],colorbar=True, cmap='jet', vmax=5.5,  title='{}_diffusion_map_{}_rh'.format(prefix,indiv_emb_state),output_file=image_folder+'/{}_diffusion_map_{}_rh.png'.format(prefix,emb_condition))
+        # print("Gradient image for %s completed" % mat_file)
+
+        
+        if plot_n_dims > n_dims:
+            plot_n_dims = n_dims #control if nb of dims > actual nb
+
+        print('Creating images for {} dims'.format(plot_n_dims))
+        for dim in range(plot_n_dims):
+
+            image_folder_dims = image_folder+'/Gradient_{}'.format(dim+1) 
+            if not os.path.isdir(image_folder_dims):
+                os.makedirs(image_folder_dims)
+
+            a = np.zeros(20484) #size concatenated vertices from fsaverage5 template
+            mean_embs = np.mean(b['emb'],axis=0) #mean embeddings for each dimension, across subjects (axis=0 signifies avg across subjects, which is the first dimension of b)
+            a[lab]=np.mean(b['emb'],axis=0)[:,dim] #check if corresponds to 1st gradient
+            nilearn.plotting.plot_surf_stat_map('/mnt/data/romy/packages/freesurfer/subjects/fsaverage5/surf/lh.inflated',a[:10242],colorbar=True, cmap='jet', vmax=5.5,title='{}_diffusion_map_{}_lh_DIMENSION#{}'.format(prefix,indiv_emb_state,dim+1),output_file=image_folder_dims+'/{}_diffusion_map_{}_lh_Dim#{}.png'.format(prefix,indiv_emb_state,dim+1))
+            nilearn.plotting.plot_surf_stat_map('/mnt/data/romy/packages/freesurfer/subjects/fsaverage5/surf/rh.inflated',a[10242:],colorbar=True, cmap='jet', vmax=5.5, title='{}_diffusion_map_{}_rh_DIMENSION#{}'.format(prefix,indiv_emb_state,dim+1),output_file=image_folder_dims+'/{}_diffusion_map_{}_rh_Dim#{}.png'.format(prefix,indiv_emb_state,dim+1))
+            print("Gradient image for %s completed" % mat_file)
+
+
+    except:
+        print("/!\ Gradient image for %s failed /!\ " % mat_file)
+
 
 
 """
@@ -115,7 +201,13 @@ Launch the script to get gradient images for group level analysis (have to selec
 
 
 
-    
+"""
+Option 2 : launch for individual embedding conditions (not mixed)
+"""
+indiv_emb_states = emb_condition.split('_')
+print(indiv_emb_states)
+for indiv_emb_state in indiv_emb_states:
+  make_gradients_images_isolated_condition(sublist,indiv_emb_state,plot_n_dims=5)
 
 
 
