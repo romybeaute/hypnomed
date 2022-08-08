00cc863c01f905b17f256dd631940a18773dafa3
romybeaute <romybeaute.univ@gmail.com>
2022-08-08 18:00:41 +0200

    plothelp

--- diffusion_embedding/helper_functions/plot_helpers.py
+++ diffusion_embedding/helper_functions/plot_helpers.py
@@ -1,5 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import os
+from PIL import Image
+import matplotlib.image as mpimg 
+
 
 def img_is_color(img):
 
@@ -75,3 +79,92 @@ def show_image_list(list_images, list_titles=None, list_cmaps=None, grid=True, n
 
     fig.tight_layout()
     _ = plt.show()
+
+
+
+
+def side2side(images_path,output_title):
+
+
+    list_imgs = os.listdir(images_path)
+    list_titles = [list_imgs[t].split('.')[0] for t in range(len(list_imgs))]
+    path_images = [os.path.join(images_path,list_imgs[im]) for im in range(len(list_imgs))]
+
+
+    my_dpi = 300
+    fig = plt.figure(figsize=(15, 15), dpi=my_dpi)
+
+    # ================================= STATES =================================
+
+
+    # ============ AX1 ============ 
+    # PIL Image
+    ax1 = fig.add_subplot(2, 3, 1)
+    ax1.set_title("Control")
+    # ax1.set_xlabel('Gradient 2')
+    # ax1.set_ylabel('Gradient 1')
+    ax1.set_xticks([])
+    ax1.set_yticks([])
+    pil_img = Image.open(path_images[0])
+    ax1.imshow(pil_img)
+
+    # ============ AX1 ============ 
+    # PIL Image
+    ax2 = fig.add_subplot(2, 3, 2)
+    ax2.set_title("Hypnose")
+    # ax2.set_xlabel('X label')
+    # ax2.set_ylabel('Y label')
+    ax2.set_xticks([])
+    ax2.set_yticks([])
+    pil_img = Image.open(path_images[1])
+    ax2.imshow(pil_img)
+
+    # ============ AX2 ============ 
+    # mpimg image
+    ax3 = fig.add_subplot(2, 3, 3)
+    ax3.set_title("Meditation")
+    ax3.set_xticks([])
+    ax3.set_yticks([])
+    mpimg_img = mpimg.imread(path_images[3]) 
+    ax3.imshow(mpimg_img)
+
+
+
+    # ================================= BLOCKS =================================
+
+
+    # ============ AX1 ============ 
+    # PIL Image
+    ax4 = fig.add_subplot(2, 3, 4)
+    ax4.set_title("Run-1")
+    ax4.set_xlabel('Gradient 2')
+    ax4.set_ylabel('Gradient 1')
+    ax4.set_xticks([])
+    ax4.set_yticks([])
+    pil_img = Image.open(path_images[4])
+    ax4.imshow(pil_img)
+
+    # ============ AX1 ============ 
+    # PIL Image
+    ax5 = fig.add_subplot(2, 3, 5)
+    ax5.set_title("Run-2")
+    # ax2.set_xlabel('X label')
+    # ax2.set_ylabel('Y label')
+    ax5.set_xticks([])
+    ax5.set_yticks([])
+    pil_img = Image.open(path_images[5])
+    ax5.imshow(pil_img)
+
+    # ============ AX2 ============ 
+    # mpimg image
+    ax6 = fig.add_subplot(2, 3, 6)
+    ax6.set_title("Run-3")
+    ax6.set_xticks([])
+    ax6.set_yticks([])
+    mpimg_img = mpimg.imread(path_images[2]) 
+    ax6.imshow(mpimg_img)
+
+
+
+    impath = "/home/romy.beaute/projects/hypnomed/diffusion_embedding/visualize_emb_output/scatter_embedded"
+    fig.savefig(impath+"/scatterplots_states&blocks.jpg", dpi='figure', bbox_inches='tight')
