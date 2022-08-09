53826986a8872a72b7203f3a2f643f82e67e82da
romybeaute <romybeaute.univ@gmail.com>
2022-08-09 18:14:42 +0200

    analysis folder

---
+++ analysis/scripts_stats/gradient_range.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#%%
+"""
+Created on Mon May 30 17:20:48 2022
+
+@author: sebastien
+"""
+import numpy as np
+import os
+from embedding import embedding
+from misc_functions import *
+import matplotlib.pyplot as plt 
+import pandas as pd
+import seaborn as sns 
+
+
+def difference(x,y):
+    return (np.mean(x)-np.mean(y))
+    
+    
+def std_diff(x,y):
+    return (np.std(x)-np.std(y))
+
+
+def two_sample_bootstrap_mean(x,y,repetition=10000,alpha=0.05):
+    
+    x = np.array(x)
+    y = np.array(y)
+    
+    statistic = difference
+    test_stat, _ = statistic(x,y)
+    x_h0 = x - np.mean(x) 
+    y_y0 = y - np.mean(y) 
+    
+    stat_list = np.zeros(repetition)
+    stat_list_h0 = np.zeros(repetition)
+    
+    for rep_id in range(repetition):
+        current_x = list(np.random.choice(x,x.shape[0]))
+        current_y = list(np.random.choice(y,y.shape[0]))
+        current_stat,_ = statistic(current_x,current_y)
+        stat_list[rep_id] = current_stat
+
+        
+        current_x_h0 = list(np.random.choice(x_h0,x_h0.shape[0]))
+        current_y_h0 = list(np.random.choice(y_y0,y_y0.shape[0]))
+        current_stat_h0,_ = statistic(current_x_h0,current_y_h0)
+        stat_list_h0[rep_id] = current_stat_h0
+
+
+    CI = f'{str(np.quantile(stat_list,alpha/2).round(2))}, {str(np.quantile(stat_list,1-alpha/2).round(2))}'
+    
+    if test_stat<0:
+        test_stat = test_stat*-1
+    pval = (np.sum(np.abs(stat_list_h0)>=test_stat))/(repetition)
+
+    return test_stat ,pval, CI
+
+
+def two_sample_bootstrap_std(x,y,repetition=10000):
+    rng = np.random.default_rng()
+    x = np.array(x)
+    y = np.array(y)
+
+    test_stat = np.abs(std_diff(x,y))
+
+    stat_list = np.zeros(repetition)
+
+    for rep_id in range(repetition):
+        current_x = rng.choice(np.vstack((x,y)),x.shape[0],axis=0)
+        current_y = rng.choice(np.vstack((x,y)),y.shape[0],axis=0)
+        current_stat = std_diff(current_x,current_y)
+
+        stat_list[rep_id] = current_stat
+
+
+    pval = (np.sum(np.abs(stat_list)>=test_stat))/(repetition)
+
+    return stat_list,pval
+
+
+
+class p:
+    pass
+
+
+p.data_path = '/media/sebastien/LaCie/CosmoMVPA/scripts/data'
+p.results_path = '/media/sebastien/LaCie/CosmoMVPA/scripts/results/emb_decoding'
+p.surf_path = '/media/sebastien/LaCie/ERC_Antoine/fmriprep_preprocessing_step/recon_all_success/freesurfer/fsaverage5'
+p.DMN_mask_path = '/media/sebastien/LaCie/CosmoMVPA/scripts/data/DMN_mask'
+
+
+p.outliers = [50,73,32,65,90,102]
+
+p.radius = 1
+
+p.networks_labels = ['Vis', 'SM', 'DA', 'VA','Lim', 'FP','MTC','IFG','AG','MPFC','PMC']
+p.networks_wanted = ['Vis', 'SM', 'DA', 'VA','Lim', 'FP','MTC','IFG','AG','MPFC','PMC']
+p.order = np.array(['Vis', 'SM', 'DA', 'VA','FP','Lim','MTC','IFG','AG','PMC','MPFC'])
+
+
+states_wanted = ['OpenPresence','RestingState']
+group = 'all'
+score_wanted = 'DDS'
+
+    
+diff_emb, df_quest = load_data(p,0,states_wanted,group,score_wanted)
+diff_emb.emb = diff_emb.emb[:,diff_emb.emb[0,:]!=0]
+    
+
+exp_median_score = np.median(df_quest['DDS'][df_quest['group']=='exp'])
+nov_median_score = np.median(df_quest['DDS'][df_quest['group']=='nov'])
+
+
+high_DDS_experts_mask = duplicate_elements((df_quest['group']=='exp') & (df_quest['DDS']>=exp_median_score),len(states_wanted))
+low_DDS_experts_mask = duplicate_elements((df_quest['group']=='exp') & (df_quest['DDS']<exp_median_score),len(states_wanted))
+high_DDS_novices_mask = duplicate_elements((df_quest['group']=='nov') & (df_quest['DDS']>=nov_median_score),len(states_wanted))
+low_DDS_novices_mask = duplicate_elements((df_quest['group']=='nov') & (df_quest['DDS']<nov_median_score),len(states_wanted))
+
+high_DDS_mask = high_DDS_experts_mask + high_DDS_novices_mask
+low_DDS_mask = low_DDS_experts_mask + low_DDS_novices_mask
+
+#get gradient range for experts and novices
+range_x = np.max(diff_emb.emb[high_DDS_mask,:],1) - np.min(diff_emb.emb[high_DDS_mask,:],1)
+range_y = np.max(diff_emb.emb[low_DDS_mask,:],1) - np.min(diff_emb.emb[low_DDS_mask,:],1)
+
+#define gradients based on mask
+x = diff_emb.emb[high_DDS_mask,:]
+y = diff_emb.emb[low_DDS_mask,:]
+
+sns.histplot((np.mean(x,0), np.mean(y,0)))
+
+fig, ax = plt.subplots(figsize=(15, 7))
+stat_list,pval = two_sample_bootstrap_std(x,y)
+print(pval)
+#get variance for novices and experts
+x,y = np.var(diff_emb.emb[high_DDS_mask,:],1), np.var(diff_emb.emb[low_DDS_mask,:],1)
+
+data = {}
+data = {'data':np.hstack((x,y)),'groups':np.hstack((['High_DDS']*len(x),['Low_DDS']*len(y)))}
+df = pd.DataFrame(data)
+sns.violinplot(x="groups", y="data", data=df)
+ax.set_ylabel('Principal Gradient STD')
+
---
+++ analysis/scripts_stats/rs_stats.py
@@ -0,0 +1,11 @@
+"""
+Ressources : 
+- https://github.com/MICA-MNI/BrainStat/blob/master/brainstat/context/resting.py
+
+
+"""
+
+
+import sys
+sys.path.append('/home/romy.beaute/projects/hypnomed/analysis/BrainStat')
+from brainstat.context.resting import *
