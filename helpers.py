ef6e51fc15e902b1ce5fd480192b85c8e69af01f
romybeaute <romybeaute.univ@gmail.com>
2022-09-15 16:15:40 +0200

    helpers

---
+++ helpers.py
@@ -0,0 +1,55 @@
+import numpy as np
+import nibabel as nib
+import os, glob
+import pandas as pd
+from scipy.io import savemat
+from scipy.io import loadmat
+from nilearn import surface
+import os,sys
+from scipy.io import loadmat
+from PIL import Image
+from IPython.display import Image as im
+
+
+
+def imageCrop(filename):
+    """
+    Used in gradients_networks_yeo.ipynb
+    """
+    i1 = Image.open(filename)
+    i2 = np.array(i1)
+    i2[i2.sum(axis=2) == 255*4,:] = 0
+    i3 = i2.sum(axis=2)
+    x = np.where((i3.sum(axis=1) != 0) * 1)[0]
+    y = np.where((i3.sum(axis=0) != 0) * 1)[0]
+
+    result = Image.fromarray(i2[x.squeeze()][:,y.squeeze()])
+    result.save(filename)
+
+
+def load_template(template_path):
+    cortex = loadmat('/mnt/data/romy/hypnomed/git/data/cortex.mat')
+    cortex = np.squeeze(cortex['cortex'])
+    template = np.zeros([20484,5])
+    for grad_id in range(5):
+        template_L = surface.load_surf_data(os.path.join(template_path,'hcp.embed.grad_'+str(grad_id+1)+'.L.fsa5.func.gii'))
+        template_R = surface.load_surf_data(os.path.join(template_path,'hcp.embed.grad_'+str(grad_id+1)+'.R.fsa5.func.gii'))
+        template[:,grad_id] = np.hstack([template_L,template_R])
+    template = template[cortex,:]
+    template = template - np.mean(template,0)
+    template = template/np.std(template,0)
+    return template
+
+
+def emb_path(condition,emb_outpath):
+  return os.path.join(emb_outpath,'group_{}_embedding.mat'.format(condition))
+
+
+def load_embmat(emb_path,show_infos=True):
+  '''
+  Return the principal gradient of the .mat file localised in emb_path
+  '''
+  b = loadmat(file_name=emb_path) #.mat file
+  if show_infos:
+    print(' - shape embedding (n_subjects, n_voxels, n_dims): {}\n - n = {} subjects\n - condition : {}\n - path : {}\n'.format(b['emb'].shape,len(b['subs']),b['states'],emb_path))
+  return b,b['emb'][:,:,0]
